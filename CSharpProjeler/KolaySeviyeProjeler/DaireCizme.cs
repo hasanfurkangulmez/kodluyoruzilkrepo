109360dd7c5072b5d1237323b4d5e707b2fe7603
Hasan Furkan Gülmez <108900212+hasanfurkangulmez@users.noreply.github.com>
2023-01-21 01:24:52 +0300

    DaireCizme Guncellendi

--- CSharpProjeler/KolaySeviyeProjeler/DaireCizme.cs
+++ CSharpProjeler/KolaySeviyeProjeler/DaireCizme.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,52 @@ namespace PatikaDev.CSharpProjeler.KolaySeviyeProjeler
     {
         public DaireCizme()
         {
+            Ciz(PozitifSayiGiris());
+        }
+        public static void Ciz(int Limit)
+        {
+            for (int i = Limit / 2 + 1; i <= Limit; i++)
+            {
+                for (int j = 1; j <= Limit - i; j++) Console.Write(" ");
+                for (int j = 1; j <= 2 * i - 1; j++) Console.Write("*");
+                Console.WriteLine();
+            }
+            for (int i = (Limit % 2 == 0 ? Limit : Limit - 1); i >= Limit / 2 + 1; i--)
+                Console.WriteLine($"{new string(' ', Limit - i)}{new string('*', i * 2 - 1)}");
+        }
+        /// <summary>
+        /// Pozitif sayi girişi yapar.
+        /// Yapılmadığında tekrar döndürür.
+        /// </summary>
+        /// <returns>Pozitif sayı döndürür.</returns>
+        public static int PozitifSayiGiris()
+        {
+            int N = 0;
+            while (N <= 0)
+            {
+                Console.Write("Daire yarıcapı giriniz: ");
+                N = SayiMi(Console.ReadLine());
+                if (N <= 0) Console.WriteLine("Lütfen pozitif bir sayı giriniz!");
+            }
+            return N;
+        }
 
+        /// <summary>
+        /// Değişkene girilen karakterin sayı olup olmadığını kontrol eder.
+        /// </summary>
+        /// <param name="Sayi">Girişi yapılan sayı.</param>
+        /// <returns>Girilen sayıyı geri döndürür.</returns>
+        public static int SayiMi(string Sayi)
+        {
+            if (Sayi.Length <= 0)
+                return 0;
+            else
+            {
+                for (int i = 0; i < Sayi.Length; i++)
+                    if (!char.IsDigit(Sayi[i]))
+                        return 0;
+                return int.Parse(Sayi);
+            }
         }
     }
 }
