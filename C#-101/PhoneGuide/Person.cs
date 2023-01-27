a3e170d6d73d9bd647274beb135ea3ac8a6acf96
Hasan Furkan Gülmez <108900212+hasanfurkangulmez@users.noreply.github.com>
2023-01-27 18:28:04 +0300

    Add files via upload

---
+++ C#-101/PhoneGuide/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneGuide
+{
+    public struct Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public long PhoneNumber { get; set; }
+        public Person(string Name, string Surname, long PhoneNumber)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.PhoneNumber = PhoneNumber;
+        }
+        public override string ToString() => $"{Name},{Surname},{PhoneNumber}";
+        public void Clear()
+        {
+            Name = null;
+            Surname = null;
+            PhoneNumber = 0;
+        }
+    }
+}
